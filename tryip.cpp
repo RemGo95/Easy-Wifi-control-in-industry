ee0bdc8dbdb2ec5f6bd6795dcea6f7302bb2d9e5
Remigius <37455393+RemGo95@users.noreply.github.com>
2022-05-17 15:43:31 +0200

    Update tryip.cpp

--- tryip.cpp
+++ tryip.cpp
@@ -7,14 +7,16 @@ using namespace std;
 int tryAddress(string Address, int a, int b){
   
   for(int i = 0; i < b; i++){
-    int x = system("ping -c1 -s1"+ Address + i +  "> /dev/null 2>&1");
-  if (x==0){
-    cout<<"success"<<endl;
-    count<<"Address"+
-  }else{
-    cout<<"no connection"<<endl;
+    ActualAddress=Address;
+    ActualAddress += std::to_string(i);
+    int x = system("ping -c1 -s1"+ ActualAddress + "> /dev/null 2>&1");
+    if (x==0){
+      cout<<"success"<<endl;
+      cout<<"ActualAddress"<<endl;
+    }else{
+      cout<<"no connection"<<endl;
     
-  }
+    }
   
   
 }
